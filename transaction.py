3ad7527fb9106a3215f76de5af4482a641901f9a
Harinder <harindersinghkoranga@gmail.com>
2020-04-20 23:34:09 +0200

    Transaction class

---
+++ transaction.py
@@ -0,0 +1,11 @@
+from collections import OrderedDict
+from printable import Printable
+
+class Transaction(Printable):
+    def __init__(self, sender, recipient, amount):
+        self.sender = sender
+        self.recipient = recipient
+        self.amount = amount
+
+    def ordered_tx(self):
+        return OrderedDict([('sender', self.sender), ('recipient', self.recipient), ('amount', self.amount)])
