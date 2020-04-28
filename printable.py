f41ce31140bea0c85899cb6d2d45e9216acbb992
Harinder <harindersinghkoranga@gmail.com>
2020-04-28 15:11:10 +0200

    moved to utility/

--- printable.py
+++
@@ -1,4 +0,0 @@
-class Printable:
-        def __repr__(self):
-        #return 'Index: {}, Previous Hash: {}, Transaction: {}, Proof: {}, TimeStamp: {}'.format(self.index, self.previous_hash, self.transaction, self.proof, self.timestamp)
-            return str(self.__dict__)
